e4d7e9aa493be92d25cd84b92b6252572b56b603
oscar90y5 <oscarfernandezangulo@gmail.com>
2015-09-30 11:55:37 +0200

    añadida la clase algoritmo

---
+++ src/Algoritmo.java
@@ -0,0 +1,11 @@
+
+public class Algoritmo {
+	public static int algoritmo(int[] vector){
+		/* Calculamos h con el dni: "12425734"
+		 * sumamos 0,5 para redondear
+		 */
+		int h = (int) (Math.pow(vector.length,0.734) + 1.425 +0.5);
+		
+		
+	}
+}
