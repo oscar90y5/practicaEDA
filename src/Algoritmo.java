8191c96dc0d9bb5edfe6ee9a25b6f58775b7f663
oscar90y5 <oscarfernandezangulo@gmail.com>
2015-10-06 10:00:37 +0200

    añadido metodo ordIns o algo asi

--- src/Algoritmo.java
+++ src/Algoritmo.java
@@ -7,5 +7,22 @@ public class Algoritmo {
 		int h = (int) (Math.pow(vector.length,0.734) + 1.425 +0.5);
 		
 		
+	
 	}
-}
+	//FUNCIONA! ta probao
+	public  int[] ordInser(int[] vector){
+		int aux; // variable auxiliar para cambiar valores en los vectores
+		for(int i=0;i<vector.length;i++){
+			for(int j=i;j<vector.length;j++){
+				if(j+1<vector.length){
+					if(vector[j]>vector[j+1]){
+						
+					}
+				}
+			}
+		}
+		return vector;
+	}
+
+	
+}	
